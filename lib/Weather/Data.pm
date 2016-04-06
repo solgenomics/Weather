b74b65536497349dfef747bd98f5324bdacdf8dd
bellerbrock <bje24@cornell.edu>
2016-04-06 22:03:01 +0200

    implement metricgraphics.js timeseries graphics instead of pure d3

--- lib/Weather/Data.pm
+++ lib/Weather/Data.pm
@@ -37,18 +37,18 @@ has 'interval' => (isa => 'Str',
 
 
 
-sub get_data { 
+sub get_data {
     my $self = shift;
-    
+
     print STDERR "Processing weather data: ". join ", ", ($self->start_date(), $self->end_date(), $self->location(), $self->type())."\n";
-    
+
     my $location_row = $self->schema()->resultset("Location")->find({ name => $self->location() });
-    if (!$location_row) { 
+    if (!$location_row) {
 	return { error => "Unknown location (".$self->location().")" };
     }
 
     my $type_row = $self->schema()->resultset("Cvterm")->find( { name => $self->type() });
-    if (! $type_row) { 
+    if (! $type_row) {
 	return { error => "The type \'".$self->type()."\' is not recognized\n" };
     }
     my $type_id = $type_row->cvterm_id();
@@ -57,59 +57,31 @@ sub get_data {
     my $station_id_rs = $self->schema()->resultset("Station")->search( { 'location.name' => $location_row->name() }, { join => 'location' });
 
     my @station_ids = map { $_->station_id() } $station_id_rs->all();
-	
+
     print STDERR Dumper(\@station_ids);
 
     my $station_ids_str = join ", ", @station_ids;
 
-#    my $data = $c->model("Schema::Measurement")->search({ -and => [ 'time' => { '>' => $start_date }, 'time' => { '<' => $end_date} ], station_id => { -in => \@station_ids }}, { order_by => "extract(day from time)", columns => 'time', group_by => 'extract(day from time)' });
-
-    my $aggregate_function = "";
-    if ($self->type() eq "Temperature" || $self->type() eq "Relative Humidity" || $self->type() eq "Dew Point" || $self->type() eq "Intensity") { 
-	$aggregate_function = "avg";
-    }
-    elsif ($self->type() eq "Precipitation") { 
-	$aggregate_function = "sum";
-    }
-    else { 
-	return{ error => "Type ".$self->type()." is unknown" };
-    }
-    
-    my $q = "";
-
-    if (!$self->interval() || $self->interval() eq "day") { 
-	$q = "select extract(year from time), extract(month from time), extract (day from time), '0:00', min(value), max(value), $aggregate_function(value)  from measurement where time > ? and time <= ? and type_id=? and station_id in (?) group by extract(year from time), extract(month from time), extract(day from time) order by extract(year from time), extract(month from time), extract(day from time)";
-    }
-    elsif ($self->interval() eq "hour") { 
-	$q = "select extract(year from time), extract(month from time), extract (day from time), extract(hour from time) || ':00', min(value), max(value), $aggregate_function(value)  from measurement where time > ? and time <= ? and type_id=? and station_id in (?) group by extract(year from time), extract(month from time), extract(day from time), extract(hour from time) order by extract(year from time), extract(month from time), extract(day from time), extract(hour from time)";
-    }
+    my $q = "select time, value from measurement where time > ? and time <= ? and type_id=? and station_id in (?)";
 
     print STDERR "Query: $q\n";
 
     my $h = $self->schema()->storage()->dbh()->prepare($q);
     $h->execute($self->start_date, $self->end_date, $type_id, $station_ids_str);
     my @measurements;
-    my $avg_max = 0;
-    my @dates;
-    my $index =0;
-    while (my ($year, $month, $day, $time, $min, $max, $avg) = $h->fetchrow_array()) {
-	if ($avg > $avg_max) { $avg_max = $avg; }
-	push @measurements, { index => $index, name => "$year-$month-$day $time", value => $avg };
-	push @dates, "$year-$month-$day $time";
-	$index++;
-    }	
 
-    
+    while (my ($time, $value) = $h->fetchrow_array()) {
+			push @measurements, { date => $time, value => $value };
+    }
+
     print STDERR "Measurements: ".Dumper(\@measurements);
-    return { 
-	data => \@measurements,
-	domain_y => [ 0, $avg_max ],
-	domain_x => [ @dates ],
+    return {
+			data => \@measurements,
     };
 
 }
 
-sub calculate_sun_rise_sun_set { 
+sub calculate_sun_rise_sun_set {
     my $class = shift;
     my $location_id = shift;
 
@@ -118,47 +90,47 @@ sub calculate_sun_rise_sun_set {
     my $type_row = $schema->resultset("Cvterm")->find( { name => "Intensity" });
     my $sunrise_row = $schema->resultset("Cvterm")->find( { name => "Sunrise" });
     my $sunset_row = $schema->resultset("Cvterm")->find( { name => "Sunset" });
-    
+
     my $q = "SELECT time, value from measurement where location = ? and type_id = ?";
-    
-    
+
+
     my $h = $schema->storage()->dbh()->prepare($q);
 
     $h->execute($location_id, $type_row->cvterm_id());
 
     my $intensities = $h->fetchall_arrayref();
-    
+
     my $event = "";
     my $previous_phase = "";
-    foreach my $i (@$intensities) { 
+    foreach my $i (@$intensities) {
 	my $current_phase = "";
 	if ($i->[1] == 0) { $current_phase = "night"; }
-	else { 
-	    $current_phase = "day"; 
+	else {
+	    $current_phase = "day";
 	}
-	if ( ($current_phase ne $previous_phase) && $current_phase eq "night") { 
+	if ( ($current_phase ne $previous_phase) && $current_phase eq "night") {
 	    $event = "Sunset";
 	}
-	if ( ($current_phase ne $previous_phase) && $current_phase eq "day") { 
+	if ( ($current_phase ne $previous_phase) && $current_phase eq "day") {
 	    $event = "Sunrise";
 	}
-	
-	
-	    
+
+
+
     }
 
     my $uq = "UPDATE measurement set daylight=true WHERE time >= ? - extract(minute from '0:15'::Date) and time <= ?";
     my $qh  = $schema->storage()->dbh()->prepare($uq);
-    
+
     print STDERR "\n";
 
 }
 
-sub day_length { 
+sub day_length {
     my $self = shift;
     $self->set_type("intensity");
     my $data = $self->_get_data();
-    
+
 
 
 
--- root/static/js/Weather.js
+++ root/static/js/Weather.js
@@ -1,10 +1,10 @@
 
-function initialize_events() { 
+function initialize_events() {
 
     //alert('Initializing date_pickers...');
 
     $(function() {
-	$("#start_datepicker").datepicker( { 
+	$("#start_datepicker").datepicker( {
 	    'dateFormat' : "yy-mm-dd",
 	    changeMonth: true,
 	    numberOfMonths: 2,
@@ -13,9 +13,9 @@ function initialize_events() {
 //	    }
 	});
     });
-    
-    $(function() { 
-	$("#end_datepicker").datepicker( { 
+
+    $(function() {
+	$("#end_datepicker").datepicker( {
 	    'dateFormat' : "yy-mm-dd",
 	    changeMonth: true,
 	    numberOfMonths: 2,
@@ -26,142 +26,53 @@ function initialize_events() {
 	});
     });
 
-    $('#submit').click( function() { 
+    $('#submit').click( function() {
 	var location = $('#location_select').val();
 	var start_date = $('#start_datepicker').val();
 	var end_date = $('#end_datepicker').val();
 	var interval = $('input:radio[name=interval]:checked').val();
 	var type = $('input:radio[name=type]:checked').val();
 	//alert(start_date+ ' '+end_date + ' '+ location + ' '+ type);
-	
-	var types = new Array('Temperature', 'Relative Humidity', 'Dew Point', 'Intensity', 'Precipitation');
-	
+
+	var types = [['Temperature', ' in °C', 'Description goes here', '#8C001A'], ['Intensity', ' in lum/ftÂ²', 'Description goes here', '#ffd300'], ['Dew Point', ' in °C', 'Description goes here', '#5cb85c'], ['Relative Humidity', ' expressed as a %', 'Description goes here', '#5bc0de'], ['Precipitation', ' in mm', 'Description goes here', '#428bca']];
+
 	jQuery('#chart_area').html("");
-	for(var n = 0; n<types.length; n++) { 
+	for(var n = 0; n<types.length; n++) {
 	    //alert("Getting data for "+types[n]);
 	     data = get_data(location, start_date, end_date, interval, types[n]);
 	}
     });
 }
 
-function get_data(location, start_date, end_date, interval, type) { 
-    jQuery.ajax( { 
+function get_data(location, start_date, end_date, interval, type) {
+
+    jQuery.ajax( {
 	url: '/rest/weather',
-	data: { 'location' : location, 'start_date' : start_date, 'end_date' : end_date, 'interval' : interval, 'type' : type },
-	success: function(response) { 
-	    if (response.error) { 
+	data: { 'location' : location, 'start_date' : start_date, 'end_date' : end_date, 'interval' : interval, 'type' : type[0] },
+	success: function(response) {
+	    if (response.error) {
 		alert(response.error);
 	    }
-	    else { 
-		var s = JSON.stringify(response);
-		//alert(s);
-		
-		graph(response.data, response.domain_x, response.domain_y);
+	    else {
+    var targetdiv_id = '#'+type[0].replace(" ", "");
+    var title = type[0] + type[1];
+    var json_data = MG.convert.date(response.data, 'date', "%Y-%m-%d %H:%M:%S");
+    MG.data_graphic({
+        title: title,
+        description: type[2],
+        color: type[3],
+        data: json_data,
+        linked: true,
+        full_width: true,
+        height: 300,
+        right: 40,
+        xax_count: 4,
+        target: targetdiv_id
+    });
 	    }
 	},
-	error: function(response) { 
+	error: function(response) {
 	    alert('error');
 	}
     });
 }
-
-function graph(data) { 
-
-    var y_values = new Array();
-    var x_values = new Array();
-
-    var y_max = 0;
-
-    for (var i=0; i<data.length; i++) { 
-	if (data[i].value > y_max) { y_max = data[i].value };
-	y_values.push(data[i].value);
-	x_values.push(data[i].name);
-    }
-
-//    var svg = d3.select("#chart_area")
-//        .append("svg")
-//        .attr("width", width)
-//        .attr("height", height)
-//    ;
-
-   
-
-    var margin = {top: 20, right: 20, bottom: 70, left: 40},
-    width = 800 - margin.left - margin.right,
-    height = 140 - margin.top - margin.bottom;
-    
-    // var graph_div = d3.select("#chart_area").append('svg')
-    // 	.attr("width", width + margin.left + margin.right)
-    // 	.attr("height", height + margin.top + margin.bottom)
-    // 	.append("g")
-    // 	.attr("transform", "translate("+margin.left+', '+margin.top+")")
-    //    ;
-    // Parse the date / time
-    var	parseDate = d3.time.format("%Y-%m-%d %H:%M").parse;
-    
-    var x = d3.scale.ordinal().rangeRoundBands([0, width], .05);
-    
-    var y = d3.scale.linear().range([height, 0]);
-    
-    var xAxis = d3.svg.axis()
-	.scale(x)
-	.orient("bottom")
-	.tickFormat(d3.time.format("%Y-%m-%d"));
-    
-    var yAxis = d3.svg.axis()
-	.scale(y)
-	.orient("left")
-	.ticks(5);
-    
-    var svg = d3.select("#chart_area").append("svg")
-	.attr("width", width + margin.left + margin.right)
-	.attr("height", height + margin.top + margin.bottom)
-	.append("g")
-	.attr("transform", 
-              "translate(" + margin.left + "," + margin.top + ")");
-
-    data.forEach(function(d) {
-	//alert("Data d = "+d.name + " " + d.value);
-        d.date = parseDate(d.name);
-	//alert("Date:"+d.date);
-        d.name = +d.value;
-    });
-    
-   // alert(JSON.stringify(data));
-
-    x.domain(data.map(function(d) { return d.date; }));
-    y.domain([0, d3.max(data, function(d) { return d.value; })]);
-    
-
-    svg.append("g")
-	.attr("class", "x axis")
-	.attr("transform", "translate(0," + height + ")")
-	.call(xAxis)
-	.selectAll("text")
-	.style("text-anchor", "end")
-	.attr("dx", "-.8em")
-	.attr("dy", "-.55em")
-	.attr("transform", "rotate(-90)" );
-
-    svg.append("g")
-	.attr("class", "y axis")
-	.call(yAxis)
-	.append("text")
-	.attr("transform", "rotate(-90)")
-	.attr("y", 6)
-	.attr("dy", ".71em")
-	.style("text-anchor", "end")
-	.text(data.type);
-    
-    svg.selectAll("bar")
-	.data(data)
-    .enter().append("rect")
-	.style("fill", "steelblue")
-	.attr("x", function(d) { return x(d.date); })
-	.attr("width", x.rangeBand())
-	.attr("y", function(d) { return y(d.value); })
-	.attr("height", function(d) { return height - y(d.value); });
-   
-}
-
-
